12cd64a7e3a7ffc78fa31edfeb76807e2666fa64
adelynflowers <adelyn.flowers@gmail.com>
2023-12-02 11:00:51 -0500

    enable publishing to azure artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.gradle.internal.impldep.org.jsoup.safety.Safelist.basic
+import org.gradle.api.publish.PublishingExtension
 plugins {
 	java
+	`java-library`
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("net.linguica.maven-settings") version "0.5"
+	id("maven-publish")
 }
 
 group = "adelynflowers.github.io"
@@ -13,7 +18,32 @@ java {
 
 repositories {
 	mavenCentral()
+	maven {
+		url = uri("https://pkgs.dev.azure.com/adelynflowers/_packaging/adelynflowers/maven/v1")
+		name = "adelynflowers"
+		credentials(PasswordCredentials::class)
+	}
+
 }
+
+configure<PublishingExtension> {
+	publications {
+		publications {
+			create<MavenPublication>("maven") {
+				artifact(tasks.bootJar)
+			}
+		}
+	}
+	repositories {
+		maven {
+			url = uri("https://pkgs.dev.azure.com/adelynflowers/_packaging/adelynflowers/maven/v1")
+			name = "adelynflowers"
+			credentials(PasswordCredentials::class)
+		}
+	}
+}
+
+
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
